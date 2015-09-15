c2a4d5dd558c6550b659c18fa348eb7625206963
Felix Jensen <engineduck@posteo.de>
2015-09-15 15:58:50 +0200

    added use of midje.repl in each lein repl

--- profiles.clj
+++ profiles.clj
@@ -1,1 +1,9 @@
-{:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]]}}
+{:user {
+        :plugins [[cider/cider-nrepl "0.9.1"]
+                  [refactor-nrepl "1.1.0"]
+                  [lein-midje "3.1.3"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.7"]]
+        :repl {:dependencies [[midje "1.7.0"]]}
+        :repl-options {:init (use 'midje.repl)}}
+        }
+ }
