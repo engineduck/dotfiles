17e1d4365fbbda8c3f04943da49f7a63a316921b
Felix Jensen <engineduck@posteo.de>
2015-10-28 19:45:17 +0100

    added slamhound for ns cleanups

--- profiles.clj
+++ profiles.clj
@@ -2,5 +2,6 @@
         :plugins [[cider/cider-nrepl "0.9.1"]
                   [refactor-nrepl "1.1.0"]
                   [lein-midje "3.1.3"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.7"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.7"]
+                       [slamhound "1.5.5"]]
         }}
