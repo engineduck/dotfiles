df777e91e881e5c7285ca623445171f99547d676
Felix Jensen <engineduck@posteo.de>
2015-11-24 18:05:42 +0100

    use nrepl in repl only

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,10 @@
 {:user {
-        :plugins [[cider/cider-nrepl "0.9.1"]
+        :plugins [
                   [refactor-nrepl "1.1.0"]
                   [lein-midje "3.1.3"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.7"]
                        [slamhound "1.5.5"]]
-        }}
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+        }
+ :repl {:plugins [[cider/cider-nrepl "0.9.1"]]}
+ }
