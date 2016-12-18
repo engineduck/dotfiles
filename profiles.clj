d4ec7c4f796914f00c5af9ae679a65aa09eedc7c
Felix Jensen <engineduck@posteo.de>
2016-12-18 18:37:30 +0100

    remove tmux.conf and clojure configs

--- profiles.clj
+++
@@ -1,9 +0,0 @@
-{:user {
-        :plugins [[refactor-nrepl "1.1.0"]
-                  [lein-midje "3.2"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.7"]
-                       [slamhound "1.5.5"]]
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
-        }
- :repl {:plugins [[cider/cider-nrepl "0.9.1"]]}
- }
